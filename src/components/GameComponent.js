aa4084a00f7547accccbdd17d1dfa3c34b59ba22
Sharaf0 <mohammedsharaf@live.de>
2020-04-13 02:45:09 +0200

    Added Stars Reducer

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -1,14 +1,46 @@
-import React, { useState, useEffect, useCallback, useReducer, useMemo } from "react";
+import React, { useState, useEffect, useCallback, useReducer } from "react";
 import { getFilledArray } from "../Utils";
 import NumbersComponent from "./NumbersComponent";
 import StarsComponent from "./StarsComponent";
 import _ from "lodash";
 
+const getNextStarsCount = (numbers) => {
+  //FIXME: This function is getting called multiple times.
+  const nums = numbers.filter(number => number.status === "DEFAULT").map(n => n.number);
+
+  if (nums.length === 1)
+    return nums[0];
+
+  const res = _.shuffle(nums)
+    .slice(0, 2)
+    .reduce((sum, current) => sum + current, 0);
+  return res;
+}
+
+const starsReducer = (state, action) => {
+  switch (action.type) {
+    case "INIT":
+      return getNextStarsCount(action.numbers);
+    case "CLICKED_ON_DEFAULT":
+      return state === action.numbers
+        .filter((n) => n.status === "MARKED" || n.status === "WRONG")
+        .map((n) => n.number)
+        .reduce((sum, current) => sum + current, 0) + action.number ? getNextStarsCount(_.reject(action.numbers, { number: action.number })) : state;
+    case "CLICKED_ON_WRONG":
+      return state === action.numbers
+        .filter((n) => n.status === "MARKED" || n.status === "WRONG")
+        .map((n) => n.number)
+        .reduce((sum2, current) => sum2 + current, 0) - action.number ? getNextStarsCount(_.reject(action.numbers, { number: action.number })) : state;
+    default:
+      return state;
+  }
+}
+
 const initializeNumbersReducer = (gameLimit) => {
   return getFilledArray(gameLimit).map((n) => ({
     number: n,
     status: "DEFAULT",
-  }));;
+  }));
 }
 
 const numbersReducer = (state, action) => {
@@ -74,12 +106,12 @@ const numbersReducer = (state, action) => {
 
 const GameComponent = () => {
   //TODO: Move this Config to .json or so
-  const gameLimit = 12;
+  const gameLimit = 4;
   const starSize = 50;
   const starsHeight = 400;
   const starsWidth = 400;
   const [numbers, dispatchNumbers] = useReducer(numbersReducer, gameLimit, initializeNumbersReducer);
-  const [currentStarsCount, setCurrentStarsCount] = useState(0);
+  const [currentStarsCount, dispatchStarsCount] = useReducer(starsReducer, numbers, getNextStarsCount);
   const [isGameOver, setIsGameOver] = useState(false);
 
   useEffect(() => {
@@ -87,32 +119,22 @@ const GameComponent = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const getNextStarsCount = useCallback(() => {
-    if (numbers.length === 0)
-      return 0;
-    if (numbers.length === 1)
-      return 0;
-    return _.shuffle(
-      numbers
-        .filter(number => number.status === "DEFAULT")
-        .map(n => n.number)
-    )
-      .slice(0, 2)
-      .reduce((sum, current) => sum + current, 0);
-  }, [numbers]);
-
   const onNumberClick = useCallback((number) => {
     switch (number.status) {
       case "TAKEN":
+        dispatchStarsCount({ type: "CLICKED_ON_TAKEN", number: number.value, numbers });
         dispatchNumbers({ type: "CLICKED_ON_TAKEN", number: number.value, currentStarsCount });
         return;
       case "MARKED":
+        dispatchStarsCount({ type: "CLICKED_ON_MARKED", number: number.value, numbers });
         dispatchNumbers({ type: "CLICKED_ON_MARKED", number: number.value, currentStarsCount });
         return;
       case "WRONG":
+        dispatchStarsCount({ type: "CLICKED_ON_WRONG", number: number.value, numbers });
         dispatchNumbers({ type: "CLICKED_ON_WRONG", number: number.value, currentStarsCount });
         return;
       case "DEFAULT":
+        dispatchStarsCount({ type: "CLICKED_ON_DEFAULT", number: number.value, numbers });
         dispatchNumbers({ type: "CLICKED_ON_DEFAULT", number: number.value, currentStarsCount });
         return;
       default:
@@ -120,14 +142,13 @@ const GameComponent = () => {
           `Number status ${number.status} is not considered as a valid status`
         );
     }
-  }, [currentStarsCount]);
+  }, [numbers, currentStarsCount]);
 
   const initGame = () => {
     setIsGameOver(false);
     dispatchNumbers({ type: "INIT", gameLimit });
-    setCurrentStarsCount(getNextStarsCount());
+    dispatchStarsCount({ type: "INIT", numbers });
   };
-
   if (isGameOver)
     return (
       <div style={{ textAlign: "center" }}>
