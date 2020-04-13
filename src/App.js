2bc4b1b6ff39ffbbacdbe0873432f72c34e0e859
Sharaf0 <mohammedsharaf@live.de>
2020-04-13 16:57:23 +0200

    Showed the version

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import GameComponent from "./components/GameComponent";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import config from './config';
+import packageJson from '../package.json';
 
 function App() {
   return (
-    <GameComponent config={config} />
+    <>
+      <GameComponent config={config} />
+      <pre style={{textAlign: "center"}}>{`Version: ${packageJson.version}`}</pre>
+    </>
   );
 }
 
